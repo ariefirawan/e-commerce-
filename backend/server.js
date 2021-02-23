062ac05df61befb467c96a8b88d2d537d5b4e095
ariefirawan <ariefirawan@users.noreply.github.com>
2021-02-23 21:10:51 +0700

    handle unhandled promise rejection

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,15 @@ const dotenv = require('dotenv');
 //setting up config file
 dotenv.config({ path: 'backend/config/config.env' });
 
-
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   connectDB();
   console.log(
     `Server run on Port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+// handle unhandled promise rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down the server due to Unhandled Promise rejection');
+  server.close(() => process.exit(1));
 });
