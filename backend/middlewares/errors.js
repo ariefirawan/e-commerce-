a1ccdca45ce1f7e31a34e2632af8ab6fa4bc6aed
arief irawan <ariefirawant@gmail.com>
2021-02-24 09:49:56 +0700

    handle mongoose validation dan objectid error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,4 +1,4 @@
-// const ErrorHandler = require('../utils/errorHandler');
+const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
   err.errorCode = err.errorCode || 500;
@@ -17,6 +17,18 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     error.message = err.message;
+
+    // Wrong mongoose object ID error
+    if(err.name === 'CastError'){
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400)
+    }
+
+    // Handling mongoose Validation error
+    if(err.name === 'ValidationError'){
+      const message= Object.values(err.errors).map(value => value.message)
+      error = new ErrorHandler(message, 400)
+    }
 
     res.status(error.errorCode).json({
       success: false,
