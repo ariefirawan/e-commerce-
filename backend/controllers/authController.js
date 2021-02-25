26977b359a256003bdecbdc560f8c7a48c0c20f1
arief irawan <ariefirawant@gmail.com>
2021-02-25 12:02:50 +0700

    add jwt token and send cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 
 const ErrorHanlder = require('../utils/errorHandler');
+const sendToken = require('../utils/tokenJwt');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 //register a user
@@ -16,8 +17,32 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
   const token = user.getJwtToken();
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 201, res)
+});
+
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(
+      new ErrorHanlder('Please enter a valid email and password', 400)
+    );
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorHanlder('User not found', 401));
+  }
+
+  //checks password is correct or not
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    return next(new ErrorHanlder('Invalid Password', 401));
+  }
+
+  const token = user.getJwtToken();
+
+  sendToken(user, 201, res)
 });
