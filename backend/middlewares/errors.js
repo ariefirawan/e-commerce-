f01888ad3ad725c3c75f4ea08c3d261f14f6a434
arief irawan <ariefirawant@gmail.com>
2021-02-26 09:22:37 +0700

    handling error token

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -19,15 +19,33 @@ module.exports = (err, req, res, next) => {
     error.message = err.message;
 
     // Wrong mongoose object ID error
-    if(err.name === 'CastError'){
+    if (err.name === 'CastError') {
       const message = `Resource not found. Invalid: ${err.path}`;
-      error = new ErrorHandler(message, 400)
+      error = new ErrorHandler(message, 400);
     }
 
     // Handling mongoose Validation error
-    if(err.name === 'ValidationError'){
-      const message= Object.values(err.errors).map(value => value.message)
-      error = new ErrorHandler(message, 400)
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling wrong jwt error
+    if (err.name === 'JsonWebTokenError') {
+      const message = `Json Web Token is invalid, Try again!!`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling expired jwt error
+    if (err.name === 'TokenExpiredError') {
+      const message = `Json Web Token is Expired, Try again!!`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling mongoose duplicate user
+    if (err.code === 11000) {
+      const message = `User ${Object.keys(err.keyvalue)} is already exists`;
+      error = new ErrorHandler(message, 400);
     }
 
     res.status(error.errorCode).json({
